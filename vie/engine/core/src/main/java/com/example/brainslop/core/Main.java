28d43025264e7f0e6bad2446a12913762ab7b7be
Samuel <sammel-2@student.ltu.se>
2026-04-29 09:51:12 +0200

    test webhook

--- vie/engine/core/src/main/java/com/example/brainslop/core/Main.java
+++ vie/engine/core/src/main/java/com/example/brainslop/core/Main.java
@@ -4,5 +4,9 @@ import com.badlogic.gdx.ApplicationAdapter;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
-    
+    @Override
+    public void create() {
+        super.create();
+        System.out.println("hello world");
+    }
 }
